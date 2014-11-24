b27e2b8222a875616ff9d650099c55c0e450f8bf
Wim <github@gwaihir.net>
2014-11-24 17:46:26 +0100

    Creating the deck (five examples in) -- was indeed already implemented

---
+++ ghost-blitz/src/test/java/materials/EachItemIsTheDesiredItemTheSameNumberOfTimes.java
@@ -0,0 +1,47 @@
+package materials;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * Example (acceptance test)
+ */
+@RunWith(Parameterized.class)
+public class EachItemIsTheDesiredItemTheSameNumberOfTimes {
+	Item desiredItem;
+
+	public EachItemIsTheDesiredItemTheSameNumberOfTimes(Item desiredItem) {
+		this.desiredItem = desiredItem;
+	}
+
+	@Test
+	public void ThereShouldBe16CardsForEachDesiredItem() {
+		Deck deck = new Deck();
+		int found = 0;
+		while (deck.hasNext()) {
+			Card card = deck.draw();
+			if (card.desiredItem() == desiredItem) {
+				found++;
+			}
+		}
+		assertThat(found, is(16));
+
+	}
+
+	@Parameters
+	public static Collection<Object[]> combos() {
+		return Arrays.asList(new Object[][] { 
+				{ Item.Ghost },
+				{ Item.Chair }
+		});
+	}
+
+}
