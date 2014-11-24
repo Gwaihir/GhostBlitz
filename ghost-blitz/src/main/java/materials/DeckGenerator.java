6ce170e79496e1db554264deb13dd963428d9e9e
Wim <github@gwaihir.net>
2014-11-24 17:36:35 +0100

    Creating the deck (four examples in) -- it is probably complete now..

--- ghost-blitz/src/main/java/materials/DeckGenerator.java
+++ ghost-blitz/src/main/java/materials/DeckGenerator.java
@@ -24,10 +24,7 @@ class DeckGenerator {
 
 	private void createCardsForItemCombination(Item item1, Item item2) {
 		createCardsWithItemDepictedInOriginalColor(item1, item2);
-		for (int k = 0; k < 6; k++) {
-			cards.add(new Card(Item.Mouse, new Image(item1, Color.Blue),
-					new Image(item2, Color.Gray)));
-		}
+		createCardsWithoutItemDepictedInOriginalColor(item1, item2);
 	}
 
 	private void createCardsWithItemDepictedInOriginalColor(Item item1,
@@ -38,12 +35,37 @@ class DeckGenerator {
 				item1.color(), item2.color())), new Image(item2, item2.color())));
 	}
 
+	private void createCardsWithoutItemDepictedInOriginalColor(Item item1,
+			Item item2) {
+		ArrayList<Item> desiredItems = itemsMinus(item1, item2);
+		for (Item desiredItem : desiredItems) {
+			ArrayList<Color> colors = colorsMinus(desiredItem.color(), item1.color(), item2.color());
+			cards.add(new Card(desiredItem, new Image(item1, colors.get(0)), 
+					new Image(item2, colors.get(1))));
+			cards.add(new Card(desiredItem, new Image(item1, colors.get(1)), 
+					new Image(item2, colors.get(0))));
+		}
+
+	}
+
 	private Color aRandomColorThatIsnt(Color... skipColors) {
+		ArrayList<Color> colors = colorsMinus(skipColors);
+		return colors.get((int) Math.round(-0.5 + Math.random() * colors.size()));
+	}
+
+	private ArrayList<Color> colorsMinus(Color... skipColors) {
 		ArrayList<Color> colors = new ArrayList<Color>(Arrays.asList(Color.values()));
 		for (int i = 0; i < skipColors.length; i++) {
 			colors.remove(skipColors[i]);
 		}
-		return colors.get((int) Math.round(-0.5 + Math.random() * colors.size()));
+		return colors;
+	}
+
+	private ArrayList<Item> itemsMinus(Item item1, Item item2) {
+		ArrayList<Item> desiredItems = new ArrayList<Item>(Arrays.asList(Item.values()));
+		desiredItems.remove(item1);
+		desiredItems.remove(item2);
+		return desiredItems;
 	}
 
 }
---
+++ ghost-blitz/src/test/java/materials/EachCombinationIsUsedToLeadToEachItemNotDepictedAsTheDesiredItem.java
@@ -0,0 +1,56 @@
+package materials;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * Example (acceptance test)
+ */
+@RunWith(Parameterized.class)
+public class EachCombinationIsUsedToLeadToEachItemNotDepictedAsTheDesiredItem {
+	Item desiredItem;
+	Item item1;
+	Item item2;
+	
+	public EachCombinationIsUsedToLeadToEachItemNotDepictedAsTheDesiredItem(
+			Item desiredItem, Item item1, Item item2) {
+		this.desiredItem = desiredItem;
+		this.item1 = item1;
+		this.item2 = item2;
+	}
+
+
+
+	@Test
+	public void ThereShouldBeACardShowingItem1AndItem2ForWhichTheDesiredItemIsItem3() {
+		Deck deck = new Deck();
+		int found = 0;
+		while (deck.hasNext()) {
+			Card card = deck.draw();
+			if (card.depictsItem(item1) && card.depictsItem(item2) 
+					&& card.desiredItem() == desiredItem) {
+				found++;
+			}
+		}
+		assertThat(found, is(greaterThanOrEqualTo(1)));
+	}
+
+	@Parameters
+	public static Collection<Object[]> combos() {
+		return Arrays.asList(new Object[][] { 
+				{ Item.Bottle, Item.Ghost, Item.Chair },
+				{ Item.Mouse, Item.Ghost, Item.Chair }
+		});
+	}
+
+
+}
